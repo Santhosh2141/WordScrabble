42b12351e34cf0b1f9ed333b9b531096a6627ba1
Santhosh2141 <snthshsrinivas@gmail.com>
2022-01-19 14:17:40 +0530

    Initial Commit

---
+++ WordScamble/WordScambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScambleApp.swift
+//  WordScamble
+//
+//  Created by Santhosh Srinivas on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
